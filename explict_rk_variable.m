2622d7b83e99a91631fdf2f0e899e462b4cc864c
lilydao11 <ldao@olin.edu>
2024-10-22 20:37:41 -0400

    work on rk variable step integration

--- explict_rk_variable.m
+++ explict_rk_variable.m
@@ -17,8 +17,12 @@ h = 0.03;
 %% testing variable step
 p = 3;
 error_desired = 0.05;
+tspan = [0, 2];
+X0 = XA;
 [XB, num_evals, h_next, redo] = explicit_RK_variable_step...
 (@rate_func01,t,XA,h,DormandPrince,p,error_desired);
+[t_list,X_list,h_avg, num_evals] = explicit_RK_variable_step_integration ...
+(@rate_func01,tspan,X0,h_ref,DormandPrince,p,error_desired)
 %% LOCAL ERROR
 h_list = logspace(-5,1,100); 
 
@@ -67,6 +71,66 @@ title("Local Error vs. |XB1-XB2|")
 legend("XB1", "XB2")
 ylabel("Local Error")
 xlabel("|XB1-XB2|")
+%% RK Variable Step Integration
+%Runs numerical integration arbitrary RK method using variable time steps
+%INPUTS:
+%rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+%tspan: a two element vector [t_start,t_end] that denotes the integration endpoints
+%X0: the vector describing the initial conditions, X(t_start)
+%h_ref: the desired value of the average step size (not the actual value)
+%BT_struct: a struct that contains the Butcher tableau
+% BT_struct.A: matrix of a_{ij} values
+% BT_struct.B: vector of b_i values
+% BT_struct.C: vector of c_i values
+%p: how error scales with step size (error = k*h^p)
+%error_desired: the desired local truncation error at each step
+%OUTPUTS:
+%t_list: the vector of times, [t_start;t_1;t_2;...;.t_end] that X is approximated at
+%X_list: the vector of X, [X0’;X1’;X2’;...;(X_end)’] at each time step
+%h_avg: the average step size
+%num_evals: total number of calls made to rate_func_in during the integration
+function [t_list,X_list,h_avg, num_evals] = explicit_RK_variable_step_integration ...
+(rate_func_in,tspan,X0,h_ref,BT_struct,p,error_desired)
+    num_evals = 0;
+    h = h_ref;
+    h_list = h_ref;
+    X_list = [];
+    t_list = linspace(tspan(1),tspan(2),100); % change num_steps
+    XA = X0;
+    redo = 1;
+    % probably need a for loop for iterating through time, not sure how to
+    % do this with varying step size
+    while redo == 1
+        [XB, temp_evals, h_next, redo] = explicit_RK_variable_step...
+        (rate_func_in,t,XA,h,BT_struct,p,error_desired);
+        h = h_next;
+        num_evals = num_evals + temp_evals;
+        h_list = [h_list, h_next];
+        X_list = [X_list, XB];
+    end
+    h_avg = mean(h_list);
+
+%     % calculate steps and h
+%     [num_steps, h_avg] = iteration_solver(tspan, h_ref);
+%     % define variables
+%     XA = X0;
+%     num_evals = 0;
+%     t_list = linspace(tspan(1),tspan(2),num_steps+1);
+%  
+%     X_list = zeros(num_steps+1,length(X0));
+%     X_list(1,:) = X0';
+%     %calculate the values until it is just short of the end value
+%     for i = 1:num_steps
+%         t = t_list(i);
+%         [XB, temp_eval] = explicit_RK_step(rate_func_in,t,XA,h_avg,BT_struct);
+%         num_evals = num_evals + temp_eval;
+% 
+%         X_list(i+1,:)= XB;
+%         XA = XB;
+%     end  
+end
+
 %% RK Variable Step
 function [XB, num_evals, h_next, redo] = explicit_RK_variable_step...
 (rate_func_in,t,XA,h,BT_struct,p,error_desired)
